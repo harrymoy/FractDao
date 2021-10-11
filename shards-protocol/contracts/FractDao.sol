8d6f05bc14271c1fcb2e5794b70838703db5d6c1
Chong Kai En <kaien97@gmail.com>
2021-10-11 02:32:47 +0100

    Rewrite imports for hardhat so initial test passes

--- shards-protocol/contracts/FractDao.sol
+++ shards-protocol/contracts/FractDao.sol
@@ -1,21 +1,27 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.9;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
 import "./Settings.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 
 contract newToken is ERC20 {
-    
+
     constructor(string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
     {
-       
+
     }
-    
-    
+
+
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
@@ -24,7 +30,7 @@ contract newToken is ERC20 {
 contract FractDao is Ownable, ERC20("FractDao", "FDAO"){
     Settings private settings;
     mapping(address=>vault) public records;
-    
+
     struct vault{
         string name;
         string symbol;
@@ -32,7 +38,7 @@ contract FractDao is Ownable, ERC20("FractDao", "FDAO"){
         uint256 tokenId;
         uint256 supply;
         address newErc20Token;
-        
+
     }
     constructor(address _settings) {
         settings = Settings(_settings);
@@ -41,31 +47,29 @@ contract FractDao is Ownable, ERC20("FractDao", "FDAO"){
     function mintfdao(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
     }
-    
+
     //mint function for client
     function mint(string memory _name, string memory _symbol, address _token, uint256 _id, uint256 _supply) external returns(address) {
     require(_supply <= settings.maxSupply(), "Supply is greater than allowed");
-    
+
     //transfer ERC721 to the contract
     //IERC721(_token).setApprovalForAll(address(this), true);
     IERC721(_token).safeTransferFrom(msg.sender, address(this), _id);
-    
+
     //create new ERC20s with respective info
     newToken _newToken = new newToken(_name, _symbol);
     _newToken.mint(msg.sender, _supply);
-    
+
     //transfer token to user
     //_newToken.transfer(msg.sender, _supply);
-    
+
     //recordkeeping
     records[msg.sender] = vault(_name, _symbol, _token, _id, _supply, address(_newToken));
-    
+
     return address(_newToken);
     }
-    
+
     function onERC721Received(address _operator,address _from,uint256 _tokenId,bytes calldata _data) external pure returns(bytes4){
         return this.onERC721Received.selector;
     }
 }
-
-
--- shards-protocol/contracts/Settings.sol
+++ shards-protocol/contracts/Settings.sol
@@ -1,18 +1,19 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.9;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Settings is Ownable{
     uint public maxSupply;
-    
+
     constructor() {
         maxSupply = 10;
     }
-    
+
     function updateMaxSupply(uint _newSupply) external onlyOwner {
         maxSupply = _newSupply;
     }
-    
-    
+
+
 }
