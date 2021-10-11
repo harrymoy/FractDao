d7ff6ac47ee3bddb0c6b40cae1a2fc9fa1b03960
Chong Kai En <kaien97@gmail.com>
2021-10-11 05:23:30 +0100

    added voting system

--- shards-protocol/contracts/FractDao.sol
+++ shards-protocol/contracts/FractDao.sol
@@ -1,11 +1,7 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.9;
 
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
 import "./Settings.sol";
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -39,15 +35,42 @@ contract FractDao is Ownable, ERC20("FractDao", "FDAO"){
     }
 
     Settings private settings;
-    mapping(address=>vault) public records;
+    mapping(address => vault) public records;
 
+    address[] private _activeAddresses;
+    mapping(address=>uint256) private _snapshot;
+    mapping(address => bool)  private _votedList;
+    mapping(uint=>uint) private _votes; // private or public? private lets people make decisions without getting influenced, but may result in a long time before votes actually work
+    uint[] private _supplyVotedOn;
+    uint private _topVotedSupply;
+    uint private _mostVotesNumber;
+
+    event voteReceived(address _voter, uint _num_votes);//, uint _supply);
+    event supplyUpdated(uint _new_supply);
+    uint public voteEndBlock;
+    uint public votingCost;
 
     constructor(address _settings) {
         settings = Settings(_settings);
     }
+
+    modifier onlyGovernors {
+      require(balanceOf(msg.sender) > 0);
+      _;
+    }
+
+    // compile active addresses - for accounts just about to receive tokens
+    function _addActiveAddress(address account) internal {
+        if (balanceOf(account) == 0) {
+          _activeAddresses.push(account);
+        }
+    }
+
     //mint FDAO supply
     function mintfdao(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
+        votingCost = totalSupply() / 1000; // starting a vote costs 0.1% of total supply
+        _addActiveAddress(account);
     }
 
     //mint function for client
@@ -75,6 +98,56 @@ contract FractDao is Ownable, ERC20("FractDao", "FDAO"){
         return this.onERC721Received.selector;
     }
 
+    function _beforeTokenTransfer(address sender, address recipient, uint amount) internal override{
+        _addActiveAddress(recipient); // sender should already be recorded by the logic
+    }
+
     // Governance - vote to increase max. supply
-    //function vote(ad
+    function startVote() public onlyGovernors {
+        // burn X amount of governance tokens so voting isn't somehow abused
+        require(block.timestamp > voteEndBlock); // require previous voting round to end before starting a new vote
+        _burn(msg.sender, votingCost);
+
+        voteEndBlock = block.timestamp + 70000; // currently, it is ~6500 blocks/day, so this is about 11 days
+
+        _topVotedSupply = settings.maxSupply();
+        _mostVotesNumber =  0;
+
+        for (uint i=0; i<_activeAddresses.length; i++) {
+            _snapshot[_activeAddresses[i]] = balanceOf(_activeAddresses[i]);
+            _votedList[_activeAddresses[i]] = false;
+        }
+
+        for (uint i = 0; i < _supplyVotedOn.length; i++) {
+            _votes[_supplyVotedOn[i]] = 0;
+        }
+        delete _supplyVotedOn;
+
+    }
+
+    function vote(uint _supply) public onlyGovernors {
+        require(block.timestamp <= voteEndBlock);
+        require(settings.maxSupply() <= _supply);
+        require(!_votedList[msg.sender]); // for now, can only vote once per voting event
+
+        _votedList[msg.sender] = true; // check-effects-interaction pattern
+
+        if (_votes[_supply] == 0) {
+          _supplyVotedOn.push(_supply);
+        }
+        _votes[_supply] += _snapshot[msg.sender];
+        if (_votes[_supply] > _mostVotesNumber) {
+          _mostVotesNumber = _votes[_supply];
+          _topVotedSupply = _supply;
+        }
+
+        emit voteReceived(msg.sender, _snapshot[msg.sender]);
+    }
+
+    function endVote() public onlyGovernors {
+        require(block.timestamp > voteEndBlock);
+        require(_mostVotesNumber > (totalSupply() * 4 / 10)); // at least 40% of token holders agree
+        settings.updateMaxSupply(_topVotedSupply);
+        emit supplyUpdated(_topVotedSupply);
+    }
 }
